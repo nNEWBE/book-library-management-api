ba6cab60e08dd88fc41a031a25c12c040ea3e50e
Shuvo Chandra Deb Nath <shuvochandra999@gmail.com>
2025-11-01 07:53:45 +0600

    project initialized

---
+++ src/main/java/com/example/book_library_management/BookLibraryManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.book_library_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookLibraryManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookLibraryManagementApplication.class, args);
+	}
+
+}
